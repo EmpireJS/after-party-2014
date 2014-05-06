a88d145c3428215cef0daa77c16033e78c36faba
indexzero <charlie.robbins@gmail.com>
2014-05-06 16:55:45 -0400

    [api] Display COC.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+$(function () {
+  //
+  // ### function resetEverything ()
+  // Resets everything
+  //
+  function resetEverything() {
+    $('#put-stuff-here').remove();
+    $('#coc').show();
 
+    $('body')
+      .append('<section id="put-stuff-here"></section>');
+
+    setTimeout(function () {
+      $('#coc').hide();
+    }, 3000);
+  }
+
+  var iid = setInterval(resetEverything, 3 * 60 * 1000);
+  resetEverything();
+});
